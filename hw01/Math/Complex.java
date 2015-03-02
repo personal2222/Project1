6533b81b11782c8441f430790a07c41dd4376764
Zhengri Fan <zf002@bucknell.edu>
2015-03-02 12:43:48 -0500

    math + audio

---
+++ hw01/Math/Complex.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw01.Math;
+
+/**
+ *
+ * @author zf002
+ */
+public class Complex {
+
+    private double real;
+    private double imag;
+
+    public Complex(double real, double imag) {
+        this.imag = imag;
+        this.real = real;
+
+    }
+
+    public double magnitude() {
+        return Math.sqrt(real * real + imag * imag);
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imag;
+    }
+
+    public Complex add(Complex a) {
+        double real = this.real + a.getReal();
+        double imag = this.imag + a.getImaginary();
+        return new Complex(real, imag);
+    }
+
+    public Complex sub(Complex a) {
+        return this.add(a.negate());
+    }
+
+    public Complex negate() {
+        return new Complex(this.real, this.imag);
+    }
+
+    public Complex mul(Complex a) {
+        double real = this.real * a.getReal() - this.imag * a.getImaginary();
+        double imag = this.real * a.getImaginary() + this.imag * a.getReal();
+        return new Complex(real, imag);
+    }
+
+    public Complex mul(double a) {
+        double real = this.real * a;
+        double imag = this.imag * a;
+        return new Complex(real, imag);
+    }
+
+    public Complex exp() {
+        return new Complex(Math.exp(real) * Math.cos(this.imag), Math.exp(real) * Math.sin(this.imag));
+    }
+
+}
---
+++ hw01/Math/DFT.java
@@ -0,0 +1,41 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2015
+ *
+ * Name: Zhengri Fan
+ * Date: 2015-2-28
+ * Time: 23:24:19
+ *
+ * Project: csci205
+ * Package: hw01.Math
+ * File: DFT
+ * Description:
+ *
+ * ****************************************
+ */
+package hw01.Math;
+
+/**
+ *
+ * @author Zhengri Fan
+ */
+public class DFT {
+
+    public static Complex[] Transform(Complex[] series) {
+        int inputLength = series.length;
+        Complex[] k_array = new Complex[inputLength];
+        for (int k = 0; k < inputLength; ++k) {
+            Complex sum = new Complex(0, 0);
+            for (int t = 0; t < inputLength; ++t) {
+                sum = sum.add(exponent(t, k, inputLength).exp().mul(series[t]));
+            }
+            k_array[k] = sum;
+        }
+        return k_array;
+    }
+
+    private static Complex exponent(int t, int k, int n) {
+        double imag = (-2 * Math.PI * t * k) / n;
+        return new Complex(0, imag);
+    }
+}
