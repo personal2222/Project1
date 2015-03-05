779a6c27bca041bf26c8fe401856f7db50f5e25e
Jiayu Huang <jh054@bucknell.edu>
2015-03-04 20:09:40 -0500

    能存啦！

--- src/hw01/Sound_alt.java
+++ src/hw01/Sound_alt.java
@@ -6,11 +6,12 @@
 package hw01;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -18,14 +19,13 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.JFileChooser;
 
 /**
  *
  * @author Ruby
- * 
- * 
- * 
+ *
+ *
+ *
  * http://www.bfxr.net/
  */
 public class Sound_alt {
@@ -34,9 +34,9 @@ public class Sound_alt {
     private Clip clip;
     private Mixer.Info[] mixInfos;
     private File audioFile;
-    
-    public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException, IOException, MalformedURLException, InterruptedException{
-        File file = new File("./src/hw01/Jump.wav");
+
+    public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException, IOException, MalformedURLException, InterruptedException {
+        File file = new File("./src/hw01/Jump2.wav");
         Sound_alt sound = new Sound_alt(file);
         sound.play();
     }
@@ -46,40 +46,26 @@ public class Sound_alt {
         mixer = AudioSystem.getMixer(mixInfos[0]);
         audioFile = file;;
     }
-    
-    public void play() throws MalformedURLException, LineUnavailableException, UnsupportedAudioFileException, IOException, InterruptedException{
+
+    public void play() throws MalformedURLException, LineUnavailableException, UnsupportedAudioFileException, IOException, InterruptedException {
         DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
         try {
             clip = (Clip) mixer.getLine(dataInfo);
         } catch (LineUnavailableException e) {
             e.printStackTrace();
         }
-
+        File qwe = new File("./src/hw01/ccc.wav");
         URL soundURL = audioFile.toURI().toURL();
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
         byte[] b;
-        b = new byte[(int)audioStream.getFrameLength()*2+1];
-            System.out.println(audioStream.getFrameLength());
-            int a = audioStream.read(b);
-            System.out.print("\n");
-            //for(byte t :b){
-               // System.out.println(t);}
-           
-            
+        b = new byte[(int) audioStream.getFrameLength() * 2 + 1];
+        System.out.println(audioStream.getFrameLength());
+        int a = audioStream.read(b);
+        System.out.print("\n");
+        ByteArrayInputStream k = new ByteArrayInputStream(b);
+        AudioFormat c = audioStream.getFormat();
+        AudioInputStream out = new AudioInputStream(k, c, (int) audioStream.getFrameLength() * 2 + 1);
+        AudioSystem.write(out, AudioFileFormat.Type.WAVE, qwe);
 
-
-        
-//        clip.open(audioStream);
-//        
-//
-//        clip.start();
-//        
-//        Thread.sleep(100000);
-
-//        while (clip.isActive()){
-//            clip.start();
-//            System.out.println(clip.getFramePosition());
-//        }
-        //clip.close();
     }
 }
